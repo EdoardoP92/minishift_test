6a9aee02e88744e10c48667ac1f6f39ee88b78ba
e.patti <e.patti.guest@posteitaliane.it>
2021-02-03 12:42:09 +0100

    first commit

---
+++ src/main/java/minishift/test/MinishiftTestApplication.java
@@ -0,0 +1,13 @@
+package minishift.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinishiftTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinishiftTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minishift/test/MinishiftTestApplicationTests.java
@@ -0,0 +1,13 @@
+package minishift.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinishiftTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
