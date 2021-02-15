969057753fb36849e8694fd384a48dd3f09a0aab
e.patti <e.patti.guest@posteitaliane.it>
2021-02-15 12:58:22 +0100

    latest

--- src/main/java/minishift/test/controller/DummyController.java
+++ src/main/java/minishift/test/controller/DummyController.java
@@ -1,5 +1,7 @@
 package minishift.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DummyController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DummyController.class);
+	
 	@GetMapping
 	public ResponseEntity<String> isAlive(){
 		
 		MultiValueMap<String, String> responseHeaders = new HttpHeaders();
 		String responseBody = "isAlive()";
 		ResponseEntity<String> response = new ResponseEntity<>(responseBody, responseHeaders, HttpStatus.OK);
+		LOGGER.info("----- request received !!! -----");
 		
 		return response;
 	}
